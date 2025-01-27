c61f01f79821191ef1b0e47653687c204f377ed8
Max Abramov <gobpo2002@gmail.com>
2025-01-28 01:25:26 +0600

    Chapter 16

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -63,7 +63,7 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	}
 
 	query := `
-	id, created_at, title, year, runtime, genres, version 
+	SELECT id, created_at, title, year, runtime, genres, version 
 	FROM movies 
 	WHERE id = $1`
 
